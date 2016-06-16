e3feb4d43124a1d75f41a1b94f46e43db3c5eb67
carpres1 <carpres1@inf.upv.es>
2016-06-16 17:39:52 +0200

    visual

--- index.php
+++ index.php
@@ -104,7 +104,7 @@
 			}
 
 			// priting basic info about user on the screen
-			print_r ($allFriends);
+			print_r ($allFriends[0]["picture"]["url"]);
 		  	// Now you can redirect to another page and use the access token from $_SESSION['facebook_access_token']
 		} else {
 			$helper = $fb->getRedirectLoginHelper();

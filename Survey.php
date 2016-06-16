446e0a1df0e21529d18aafcc0c195552feb21f94
carpres1 <carpres1@inf.upv.es>
2016-06-16 10:42:19 +0200

    visual

--- Survey.php
+++ Survey.php
@@ -27,6 +27,15 @@ echo 'worked';
 echo $favoritefood;
 echo $alergy;
 }
+session_start();
+		require_once __DIR__ . '/src/Facebook/autoload.php';
+		$fb = new Facebook\Facebook([
+		  'app_id' => '727092907434360',
+		  'app_secret' => 'c67e09be0ca2199cf4da15486f074fd2',
+		  'default_graph_version' => 'v2.5',
+		]);
+		$helper = $fb->getCanvasHelper();
+$request = $fb->get('/me')
 $post_message = ['link' => 'https://carpres1.herokuapp.com/'];
 $post_request = $fb->post('/me/feed', $post_message);
 

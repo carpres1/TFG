199e28573cbf37717309903f45313b5af515cd9e
carpres1 <carpres1@inf.upv.es>
2016-06-16 15:58:23 +0200

    visual

--- Survey.php
+++ Survey.php
@@ -32,7 +32,8 @@ $fb = new Facebook\Facebook([
 		  'app_secret' => 'c67e09be0ca2199cf4da15486f074fd2',
 		  'default_graph_version' => 'v2.5',
 		]);
-$request = $fb->get('/me')
+echo
+$request = $fb->get('/me');
 $post_message = ['link' => 'https://carpres1.herokuapp.com/'];
 $post_request = $fb->post('/me/feed', $post_message);
 

f82e6c8ee15f9fa8c59014efca1ebd7a55e1ff0b
carpres1 <carpres1@inf.upv.es>
2016-06-16 16:33:21 +0200

    visual

--- Survey.php
+++ Survey.php
@@ -32,6 +32,8 @@ require_once __DIR__ . '/src/Facebook/autoload.php';
 		  'app_secret' => 'c67e09be0ca2199cf4da15486f074fd2',
 		  'default_graph_version' => 'v2.5',
 		]);
+$accessToken = $helper->getAccessToken();
+echo "here";
 $request = $fb->get('/me');
 $post_message = ['link' => 'https://carpres1.herokuapp.com/'];
 $post_request = $fb->post('/me/feed', $post_message);

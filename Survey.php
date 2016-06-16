ab51bc3d91515d0ed2227dc8d526b17ffa2497fa
carpres1 <carpres1@inf.upv.es>
2016-06-16 16:35:52 +0200

    visual

--- Survey.php
+++ Survey.php
@@ -1,30 +1,4 @@
 <?php
-$favoritefood = 0;
-$alergy =0;
-
-if (isset($_POST['Submit1'])) {
-if (!isset($_POST['favoritefood'])){
-    		echo "No Language gotten";
-    	return;
-				}
-	$selected_radio = $_POST['favoritefood'];
-	$selected_radio2 = $_POST['alergy'];
-
-	if ($selected_radio == '1') {
-
-		$favoritefood = '1';
-
-	}
-	if ($selected_radio2 == '2') {
-
-		$alergy = 2;
-		
-
-	}
-}
-echo 'worked';
-echo $favoritefood;
-echo $alergy;
 
 require_once __DIR__ . '/src/Facebook/autoload.php';
 		$fb = new Facebook\Facebook([

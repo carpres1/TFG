f808d49689025d29d0fd4df01277b10b51aec431
carpres1 <carpres1@inf.upv.es>
2016-06-16 16:38:13 +0200

    visual

--- Survey.php
+++ Survey.php
@@ -1,12 +1,39 @@
 <?php
-
 require_once __DIR__ . '/src/Facebook/autoload.php';
 		$fb = new Facebook\Facebook([
 		  'app_id' => '727092907434360',
 		  'app_secret' => 'c67e09be0ca2199cf4da15486f074fd2',
 		  'default_graph_version' => 'v2.5',
 		]);
-$accessToken = $helper->getAccessToken();
+		$helper = $fb->getCanvasHelper();
+		$permissions = ['email','publish_actions','user_friends','user_hometown','user_location']; // optionnal
+		try {
+			if (isset($_SESSION['facebook_access_token'])) {
+			$accessToken = $_SESSION['facebook_access_token'];
+			} else {
+		  		$accessToken = $helper->getAccessToken();
+			}
+		} catch(Facebook\Exceptions\FacebookResponseException $e) {
+		 	// When Graph returns an error
+		 	echo 'Graph returned an error: ' . $e->getMessage();
+		  	exit;
+		} catch(Facebook\Exceptions\FacebookSDKException $e) {
+		 	// When validation fails or other local issues
+			echo 'Facebook SDK returned an error: ' . $e->getMessage();
+		  	exit;
+		 }
+		if (isset($accessToken)) {
+			if (isset($_SESSION['facebook_access_token'])) {
+				$fb->setDefaultAccessToken($_SESSION['facebook_access_token']);
+			} else {
+				$_SESSION['facebook_access_token'] = (string) $accessToken;
+			  	// OAuth 2.0 client handler
+				$oAuth2Client = $fb->getOAuth2Client();
+				// Exchanges a short-lived access token for a long-lived one
+				$longLivedAccessToken = $oAuth2Client->getLongLivedAccessToken($_SESSION['facebook_access_token']);
+				$_SESSION['facebook_access_token'] = (string) $longLivedAccessToken;
+				$fb->setDefaultAccessToken($_SESSION['facebook_access_token']);
+			}
 echo "here";
 $request = $fb->get('/me');
 $post_message = ['link' => 'https://carpres1.herokuapp.com/'];
